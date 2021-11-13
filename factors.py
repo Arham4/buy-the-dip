57b468be2d5854cd38542c667f5d9059a6c9a8fe
Arham J. Siddiqui <arhamjsiddiqui@gmail.com>
2021-11-13 15:56:57 -0600

    Implement CNN fear & greed index

--- factors.py
+++ factors.py
@@ -1,8 +1,20 @@
-def stock_fear_index(ticker):
-    return 100  # dummy value
+import fear_and_greed
 
 
-def crypto_fear_index(ticker):
+def stock_fear_index():
+    """Gets the stock fear index as per CNN's website.
+
+    :returns a number from 0 to 100.
+             0 - 24 = extremely fearful
+             25 - 49 = fear
+             50 = neutral
+             51 - 74 = greed
+             75 - 100 = extremely greedy
+    """
+    return fear_and_greed.get().value
+
+
+def crypto_fear_index():
     return 100  # dummy value
 
 
