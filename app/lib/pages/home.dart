cfb08f8d9c3f5a183298f8f060c1ce4eb458a534
Arham J. Siddiqui <arhamjsiddiqui@gmail.com>
2021-11-14 10:32:28 -0600

    Make scrolling interface

--- app/lib/pages/home.dart
+++ app/lib/pages/home.dart
@@ -13,35 +13,106 @@ class Home extends StatelessWidget {
             child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  Container(
-                    height: 150,
-                    child: ListView(
-                      scrollDirection: Axis.horizontal,
-                      children: <Widget>[
-
-                      ],
-                    ),
-                  ),
-                  ElevatedButton(
-                      onPressed: () {
-                        Navigator.pushNamed(context, '/dashboard');
-                      },
-                      child: const Text('Dashboard')),
-                  ElevatedButton(
-                      onPressed: () {
-                        Navigator.pushNamed(context, '/recommendations');
-                      },
-                      child: const Text('Recommendations')),
-                  ElevatedButton(
-                      onPressed: () {
-                        Navigator.pushNamed(context, '/highestperformers');
-                      },
-                      child: const Text('Highest Performers')),
-                  ElevatedButton(
-                      onPressed: () {
-                        Navigator.pushNamed(context, '/stocks');
-                      },
-                      child: const Text('Stocks')),
-                ])));
+              Text(
+                'Recommended Stocks',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 100,
+                width: double.infinity,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: <Widget>[
+                    for (var i = 0; i < 5; i++)
+                      Card(
+                        child: SizedBox(
+                          height: 100,
+                          width: 100,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(20.0),
+                                child: Image.network(
+                                  'https://static.finnhub.io/logo/87cb30d8-80df-11ea-8951-00000000092a.png',
+                                  height: 35.0,
+                                  width: 35.0,
+                                ),
+                              ),
+                              Text(
+                                'AAPL',
+                                style: TextStyle(color: Colors.black),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                  ],
+                ),
+              ),
+              const Divider(
+                height: 15,
+                thickness: 1.0,
+              ),
+              Text(
+                'Top Performers',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 100,
+                width: double.infinity,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: <Widget>[
+                    for (var i = 0; i < 5; i++)
+                      Card(
+                        child: SizedBox(
+                          height: 100,
+                          width: 100,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(20.0),
+                                child: Image.network(
+                                  'https://static.finnhub.io/logo/87cb30d8-80df-11ea-8951-00000000092a.png',
+                                  height: 35.0,
+                                  width: 35.0,
+                                ),
+                              ),
+                              Text(
+                                'AAPL',
+                                style: TextStyle(color: Colors.black),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                  ],
+                ),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/dashboard');
+                  },
+                  child: const Text('Dashboard')),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/recommendations');
+                  },
+                  child: const Text('Recommendations')),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/highestperformers');
+                  },
+                  child: const Text('Highest Performers')),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/stocks');
+                  },
+                  child: const Text('Stocks')),
+            ])));
   }
 }
